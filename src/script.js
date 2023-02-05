652bcaf587a35579d02da9c65913f0a3df472405
Eli-senda <ventura.jariod@gmail.com>
2023-02-05 10:28:30 +0100

    Created script.js

---
+++ src/script.js
@@ -0,0 +1,66 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayWeek = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayWeek];
+  return `${day} ${hours}:${minutes}`;
+}
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+function searchCity(city) {
+  let key = "47c9e98931d04f7eee8a997938080cf4";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+function searchLocation(position) {
+  let key = "47c9e98931d04f7eee8a997938080cf4";
+  let latitude = "position.coords.latitude";
+  let longitude = "position.coords.longitude";
+  let apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=${units}";
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let dateElement = document.querySelector("#current-time");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("click", handleSubmit);
+let currentLocationButton = document.querySelector(".current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Barcelona");
